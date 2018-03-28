d0f89116ce6d3678963e4c29036949448a5741a8
t8lv4 <morgan@t8lv4.fr>
2018-03-28 10:04:29 +0200

    Initial Commit

---
+++ OpenQuizz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenQuizz
+//
+//  Created by Morgan on 28/03/2018.
+//  Copyright © 2018 Morgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
